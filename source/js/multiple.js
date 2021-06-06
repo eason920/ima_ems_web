60792fde3e529447ab773c42e63ef9c56b962b9d
eason920 <eason920@gmail.com>
2021-06-07 00:22:37 +0800

    1100606-2

--- source/js/multiple.js
+++ source/js/multiple.js
@@ -1,3 +1,44 @@
+const fnHCol = function(from){
+	let h = '';
+	h+=		'<div class="col" data-unit="' + from + '">'
+	h+=			'<div class="col-item"></div>'
+	h+=			'<div class="col-limit"></div>'
+	h+=		'</div>' // from
+	return h
+}
+
 $(()=>{
 	console.log('got multiple.js');
+	$.ajax({
+		type: 'GET',
+		url: './data/page1/main_1.json',
+		dataType: 'json',
+		success: function(res){
+			console.log(res);
+
+			let h = '';
+			for( build in res.data ){
+				h+='<div class="build">'
+				h+=	'<div class="build-title">' + res.data[build].build +'</div>'
+				h+=	'<div class="build-body">'
+				// G
+				h+= fnHCol('from')
+				// C
+				h+= fnHCol('chiller')
+				// M
+				h+= fnHCol('motor')
+				// P
+				h+= fnHCol('pipe')
+				// S
+				h+= fnHCol('switch')
+				// p
+				h+= fnHCol('phy')
+				// GCMPSP END
+				h+=	'</div>' // .build-body
+				h+='</div><br>' // .build
+			}
+
+			$('#wrapper-single').html(h);
+		}
+	})
 })
